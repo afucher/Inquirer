4d896e23e4ea9c5a988b846e0cfda76baa18ff90
Arthur Fücher <arthur.fucher@lambda3.com.br>
2017-09-22 17:10:14 -0300

    created sample project

---
+++ samples/Samples/Basic/Sample1.cs
@@ -0,0 +1,27 @@
+﻿using InquirerCore;
+using InquirerCore.Prompts;
+using InquirerCore.Validators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Samples.Basic
+{
+    public static class Sample1
+    {
+        public static void Run()
+        {
+            var numbersOnly = new RegexValidator("^[0-9]*$");
+            var nameInput = new Input("name", "What is your name?", new ConsoleRender());
+            var ageInput = new Input("age", "What is your age?", new ConsoleRender());
+            ageInput.SetValid(numbersOnly);
+
+            var inquirer = new Inquirer(nameInput, ageInput);
+
+            inquirer.Ask();
+
+            System.Console.WriteLine($@"Hello {nameInput.Answer()}! Your age is {ageInput.Answer()}");
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ samples/Samples/Program.cs
@@ -0,0 +1,15 @@
+﻿using InquirerCore;
+using InquirerCore.Prompts;
+using InquirerCore.Validators;
+using System;
+
+namespace Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Basic.Sample1.Run();
+        }
+    }
+}
