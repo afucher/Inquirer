ccdc164108e3b94525510a656c922365011dc7b4
Arthur Fücher <arthurfucher@gmail.com>
2020-06-21 22:15:54 -0300

    create numeric validator

---
+++ src/Inquirer/Validators/NumericValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace InquirerCore.Validators
+{
+    public class NumericValidator : IValidator
+    {
+        public bool Validate(string value)
+        {
+            try
+            {
+                float.Parse(value);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ test/InquirerUnitTest/Validators/NumericValidatorUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using InquirerCore.Validators;
+using Xunit;
+
+namespace InquirerUnitTest.Validators
+{
+    public class NumericValidatorUnitTest
+    {
+        [Theory]
+        [InlineData("0")]
+        [InlineData("7846")]
+        [InlineData("-8476")]
+        [InlineData("8.5")]
+        [InlineData("-0.3")]
+        public void ShouldAcceptNumericValues(string aNumericValue)
+        {
+            var validator = new NumericValidator();
+
+            var isValid = validator.Validate(aNumericValue);
+
+            isValid.Should().BeTrue();
+        }
+        
+        [Theory]
+        [InlineData("")]
+        [InlineData("sjdkh")]
+        [InlineData("32hjh32")]
+        [InlineData("-")]
+        public void ShouldNotAcceptNonNumericValues(string aNonNumericValue)
+        {
+            var validator = new NumericValidator();
+
+            var isValid = validator.Validate(aNonNumericValue);
+
+            isValid.Should().BeFalse();
+        }
+    }
+}
