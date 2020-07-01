5457fc25141b791d8ee75c1dcc5db99ba11d72d0
Arthur Fücher <arthurfucher@gmail.com>
2020-06-30 22:36:00 -0300

    remove comments from code used to show better the refactor in the live code

--- src/Inquirer/Prompts/Input.cs
+++ src/Inquirer/Prompts/Input.cs
@@ -14,22 +14,18 @@
 
         public override void Ask()
         {
-            //Create variable for position
             int[,] pos = null;
-            //Validation loop
             do
             {
-                //Clean console
                 if(pos != null)
                     consoleRender.Clean(pos[0, 1], pos[1, 1]);
-                //Render question
+
                 pos = Render();
-                //Get user answer  !!!!
+
                 _answer = GetUserAnswer();
 
             } while (!IsValidAnswer(_answer));
             
-            //Add new line
             consoleRender.Newline();
         }
         
