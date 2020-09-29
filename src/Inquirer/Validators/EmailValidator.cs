6933ebb5e06ea98c940ee1afc12f2242c36fb028
Calvin Rapier <cerapier@gmail.com>
2020-09-29 00:13:38 -0700

    Add EmailValidator

---
+++ src/Inquirer/Validators/EmailValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace InquirerCore.Validators
+{
+    public class EmailValidator : IValidator
+    {
+        private string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+        
+        public bool Validate(string value)
+        {
+            try
+            {
+                return Regex.IsMatch(value, emailPattern, RegexOptions.IgnoreCase,TimeSpan.FromMilliseconds(250));
+            } catch(Exception e)
+            {
+                return false;
+            }
+        }
+
+        public string GetErrorMessage()
+        {
+            return "Invalid email address";
+        }
+    }
+}
