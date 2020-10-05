23b006af0ab87819938e634bb7c1531711f9f736
Fabio Silva Garbin <fgarbin@gmail.com>
2020-10-05 00:30:42 -0300

    Add RangeValidator and test.

---
+++ src/Inquirer/Validators/RangeValidator.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+
+namespace InquirerCore.Validators
+{
+    public class RangeValidator : IValidator
+    {
+    private int iMin { set; get; }
+    private int iMax { set; get; }
+
+        public RangeValidator(int _iMin, int _iMax)
+        {
+          iMin = _iMin;
+          iMax = _iMax;
+        }
+
+        public bool Validate(string value)
+        {
+            try
+            {
+              int iVal = int.Parse(value);
+              return iVal >= iMin && iVal <= iMax;
+            }
+            catch (Exception e)
+            {
+              return false;
+            }
+        }
+
+        public string GetErrorMessage()
+        {
+        return String.Format("Answer accepts between {0} to {1}.", iMin, iMax);
+        }
+    }
+}
