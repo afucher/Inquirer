9593c620fd4af173f44ba0cd78772c27fae4fcd9
Fábio Silva Garbin <fgarbin@gmail.com>
2020-10-16 09:31:55 -0300

    Add RangeValidator and test. (#21)
    * Add RangeValidator and test.
    
    * Update code conventions
    
    * Update test message.
    
    * Correct variable declaration and remove comments.

---
+++ src/Inquirer/Validators/RangeValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InquirerCore.Validators
+{
+  public class RangeValidator : IValidator
+  {
+    private int Min { set; get; }
+    private int Max { set; get; }
+
+    public RangeValidator(int minValue, int maxValue)
+    {
+      Min = minValue;
+      Max = maxValue;
+    }
+
+    public bool Validate(string value)
+    {
+      try
+      {
+        int val = int.Parse(value);
+        return val >= Min && val <= Max;
+      }
+      catch (Exception e)
+      {
+        return false;
+      }
+    }
+
+    public string GetErrorMessage()
+    {
+      return $"Answer accepts between {Min} to {Max}.";
+    }
+  }
+}
