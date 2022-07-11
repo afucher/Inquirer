20676b662ce0b9c3d896d3f5bf29811ab34ef01e
Lucas Aguiar <lucas.abreu@redeceler.com.br>
2022-07-11 08:52:50 -0300

    Add boolean validator

---
+++ test/InquirerUnitTest/Validators/BooleanValidatorUnitTest.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using InquirerCore.Validators;
+using Xunit;
+
+namespace InquirerUnitTest.Validators
+{
+    public class BooleanValidatorUnitTest
+    {
+        [Theory]
+        [InlineData("true")]
+        [InlineData("True")]
+        [InlineData("false")]
+        [InlineData("False")]
+        [InlineData("TRUE")]
+        [InlineData("FALSE")]
+        public void ShouldAcceptBooleanValues(string aBooleanValue)
+        {
+            var validator = new BooleanValidator();
+
+            var isValid = validator.Validate(aBooleanValue);
+
+            isValid.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("1")]
+        [InlineData("0")]
+        [InlineData(" ")]
+        [InlineData("invalid")]
+        [InlineData("")]
+        [InlineData(null)]
+        public void ShouldNotAcceptNonBooleanValues(string aBooleanValue)
+        {
+            var validator = new BooleanValidator();
+
+            var isValid = validator.Validate(aBooleanValue);
+
+            isValid.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShouldReturnErrorMessage()
+        {
+            var validator = new BooleanValidator();
+
+            validator.GetErrorMessage().Should().Be("Answer accepts only valid boolean values.");
+        }
+    }
+}
